ba30214f1a0d7df93687948b2308bb4239f09360
Sayam <sayamalvi07@gmail.com>
2022-06-11 23:22:54 +0530

    Revise ejs

--- EJS/ToDoListv1/app.js
+++ EJS/ToDoListv1/app.js
@@ -1,44 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const res = require('express/lib/response');
-const req = require('express/lib/request');
-
 const app = express();
-
-let items = [];
-let workItems = [];
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'))
 
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    const today = new Date();
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    };
-    const day = today.toLocaleDateString('en-IN', options);
-    res.render('list', { listTitle: day, newListItems: items });
-});
-
-app.post('/', function (req, res) {
-    let item = req.body.newItem;
-    if (req.body.list === 'Work') {
-        workItems.push(item);
-        res.redirect('/work');
-        console.log(req.body);
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = '';
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend !";
+        res.render('list', { kindOfDay: day });
     }
     else {
-        items.push(item);
-        res.redirect('/');
+        day = "Weekday.";
     }
 });
 
-app.get('/work', function (req, res) {
-    res.render('list', { listTitle: 'Work List', newListItems: workItems })
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -65,5 +60,5 @@ app.get('/work', function (req, res) {
 
 
 app.listen(3000, function () {
-    console.log('server running on port 3000')
-})
+    console.log('Server started at port 3000');
+});
