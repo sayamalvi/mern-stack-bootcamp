5ce420bdb729ad32dd86dd62ab6bdf18414284ee
Sayam <sayamalvi07@gmail.com>
2022-06-12 16:30:04 +0530

    Complete todo list v1 (ejs module)

--- EJS/ToDoListv1/app.js
+++ EJS/ToDoListv1/app.js
@@ -1,25 +1,41 @@
+const items = [];
+const workItems = [];
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const res = require('express/lib/response');
+const date = require(__dirname + '/date.js')
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = '';
-    if (currentDay === 6 || currentDay === 0) {
-        day = "Weekend !";
-        res.render('list', { kindOfDay: day });
-    }
-    else {
-        day = "Weekday.";
-    }
+    const day = date.getDate();
+    res.render("list", { listTitle: day, newListItems: items });
 });
 
+app.post('/', function (req, res) {
+    let item = req.body.newItem;
+    if (req.body.list === 'Work') {
+        workItems.push(item);
+        res.redirect('/work');
+    }
+    else {
+        items.push(item);
+        res.redirect('/');
+    }
 
+});
 
+app.get('/work', function (req, res) {
+    res.render('list', { listTitle: 'Work List', newListItems: workItems })
+});
+
+app.get('/about', function (req, res) {
+    res.render('about');
+})
 
 
 
