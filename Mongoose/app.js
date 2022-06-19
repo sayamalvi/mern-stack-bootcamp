a6d5d4879810b88e92dbaddae434eed9ef20b766
Sayam <sayamalvi07@gmail.com>
2022-06-19 20:11:01 +0530

    Add gitignore,procfile and update ports

--- Mongoose/app.js
+++ Mongoose/app.js
@@ -198,40 +198,11 @@ app.post('/delete', function (req, res) {
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function () {
-    console.log('Server started at port 3000');
+app.listen(port, function () {
+    console.log(`Server started at ${port}`);
 });
